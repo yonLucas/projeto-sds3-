20a6f0f2e4026668b312d733767e699aed303232
Lucas Ramos <lucasa7xadriano@outlook.com>
2021-05-05 16:01:13 -0300

    Project create

---
+++ projetosds3/backend/src/main/java/com/LRamos/LRVendas/LrVendasApplication.java
@@ -0,0 +1,13 @@
+package com.LRamos.LRVendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LrVendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LrVendasApplication.class, args);
+	}
+
+}
---
+++ projetosds3/backend/src/test/java/com/LRamos/LRVendas/LrVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LRamos.LRVendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LrVendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
